b22d77bbe7f320ae56d4c5f20954aaee690fc03e
Alex Alikhani <alikhanialex@gmail.com>
2022-07-03 17:21:02 +0100

    Using async with teh goalcontroller functions as a promise will be sent when mongoose is used with the goalcontroller functions. Installing async handler to avoid using try/catches and just use the err handlers

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,35 +1,37 @@
+const asyncHandler = require("express-async-handler");
+
 // Get goals
 // route - GET /api/goals
 // The access is private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get goals" });
-};
+});
 
 // Set goal
 // route - POST /api/goals
 // The access is private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     // If no body text, create a bad request status
     res.status(400);
     throw new Error("Please add a text field");
   }
   res.status(200).json({ message: "Set goals" }); // Route to create a goal
-};
+});
 
 // Update Goal
 // route - PUT /api/goals/:id
 // The access is private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update goal ${req.params.id}` });
-};
+});
 
 // Delete goal
 // route - DELETE /api/goals/:id
 // The access is private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete goal ${req.params.id}` });
-};
+});
 
 module.exports = {
   getGoals,
