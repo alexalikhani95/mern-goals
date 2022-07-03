aab7c8ff4a0b3de9a8499e2d71c6d9af4aea3a8f
Alex Alikhani <alikhanialex@gmail.com>
2022-07-03 16:10:52 +0100

    Creating post, put and delete routes for a goal

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -5,4 +5,17 @@ router.get("/", (req, res) => {
   res.send({ message: "Get goals" });
 });
 
+router.post("/", (req, res) => {
+  res.send({ message: "Set goals" }); // Route to create a goal
+});
+
+router.put("/:id", (req, res) => {
+  // Put requests need an ID
+  res.send({ message: `Update goal ${req.params.id}` });
+});
+
+router.get("/", (req, res) => {
+  res.send({ message: "Get goals" });
+});
+
 module.exports = router;
