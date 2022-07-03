1e320cce3a2d0aaa2d0a76799d64e8e364dc0ca0
Alex Alikhani <alikhanialex@gmail.com>
2022-07-03 17:47:01 +0100

    Connecting the server with mongoose

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  // All mongoose methods are asynchronous (return a promise)
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
