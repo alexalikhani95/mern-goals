cdbd5cc4f01ac1ddbd3a3933174fc25d62a70998
Alex Alikhani <alikhanialex@gmail.com>
2022-07-03 15:58:25 +0100

    Setting a route with a message to check it works

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 
+app.get("/api/goals", (req, res) => {
+  res.send({ message: "Get goals" });
+});
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
