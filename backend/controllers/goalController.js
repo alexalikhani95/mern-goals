6bf33667d3260c2846b795f29ae80e9f302ebf7e
Alex Alikhani <alikhanialex@gmail.com>
2022-07-03 18:02:52 +0100

    Adding ability to update a goal and delete a goal

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -32,14 +32,32 @@ const setGoal = asyncHandler(async (req, res) => {
 // route - PUT /api/goals/:id
 // The access is private
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true }); // req.body will be the updated text
+
+  res.status(200).json(updatedGoal);
 });
 
 // Delete goal
 // route - DELETE /api/goals/:id
 // The access is private
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  await goal.remove();
+
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
