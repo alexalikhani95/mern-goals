d3fe22b57787676f01896ff9831556c1b15a26f0
Alex Alikhani <alikhanialex@gmail.com>
2022-07-04 19:50:55 +0100

    Installing bcryptjs for passwords and jsonwebtokens for registration. Also adding async to work with mongoose and asynchandler toHandle exceptions

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,23 +1,28 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
 // Register new user
 // POST /api/users
 // access is public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
   res.json({ message: "Register User" });
-};
+});
 
 // Authenticate a user
 // POST /api/users/login
 // access is public
-const loginUser = (req, res) => {
+const loginUser = asyncHanlder(async (req, res) => {
   res.json({ message: "Login User" });
-};
+});
 
 // Get user data
 // GET /api/users/me
 // access is public
-const getMe = (req, res) => {
+const getMe = asyncHanlder(async (req, res) => {
   res.json({ message: "User data display" });
-};
+});
 
 module.exports = {
   registerUser,
